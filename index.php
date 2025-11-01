cf3b648fa41badf8b4565f84c86ec5f1fa958634
husanswe <sulaymonovhusan2002@gmail.com>
2025-11-01 11:17:13 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+    $number = filter_input(INPUT_GET, "num", FILTER_SANITIZE_NUMBER_INT);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/main.min.css">
+        <title>Multiplication Flash Cards</title>
+    </head> 
+    
+    <body>
+        <?php require("./view/header.php"); ?>
+        <?php 
+            if ($number) {
+                require("view/results.php");
+            } else {
+                require("view/form.php");
+            }
+        ?>
+        <?php require("./view/footer.php"); ?>
+    </body>
+</html>
---
+++ view/header.php
@@ -0,0 +1,5 @@
+<header class="header">
+    <h1>
+        <a href="index.php">Multiplication Flash Cards</a>
+    </h1>
+</header>
